41cdcb628bdec098aec886058fdbb99cbd91ca7c
Ed Zynda <ezynda3@gmail.com>
2024-02-28 17:00:36 +0300

    Add all known apis from viem package

--- main.go
+++ main.go
@@ -64,47 +64,6 @@ type SourcifyResponse struct {
 	Version int      `json:"version"`
 }
 
-var etherscanConfig map[string]string = map[string]string{
-	"1":         "https://api.etherscan.io/api",
-	"5":         "https://api-goerli.etherscan.io/api",
-	"11155111":  "https://api-sepolia.etherscan.io/api",
-	"100":       "https://api.gnosisscan.io/api",
-	"137":       "https://api.polygonscan.com/api",
-	"80001":     "https://api-testnet.polygonscan.com/api",
-	"56":        "https://api.bscscan.com/api",
-	"97":        "https://api-testnet.bscscan.com/api",
-	"43114":     "https://api.snowtrace.io/api",
-	"43113":     "https://api-testnet.snowtrace.io/api",
-	"10":        "https://api-optimistic.etherscan.io/api",
-	"420":       "https://api-goerli-optimistic.etherscan.io/api",
-	"42161":     "https://api.arbiscan.io/api",
-	"421613":    "https://api-goerli.arbiscan.io/api",
-	"421614":    "https://api-sepolia.arbiscan.io/api",
-	"42170":     "https://api-nova.arbiscan.io/api",
-	"250":       "https://api.ftmscan.com/api",
-	"4002":      "https://api-testnet.ftmscan.com/api",
-	"1284":      "https://api-moonbeam.moonscan.io/api",
-	"1287":      "https://api-moonbase.moonscan.io/api",
-	"1285":      "https://api-moonriver.moonscan.io/api",
-	"25":        "https://api.cronoscan.com/api",
-	"338":       "https://api-testnet.cronoscan.com/api",
-	"42220":     "https://api.celoscan.io/api",
-	"44787":     "https://api-alfajores.celoscan.io/api",
-	"288":       "https://api.bobascan.com/api",
-	"2888":      "https://api-testnet.bobascan.com/api",
-	"534353":    "https://blockscout.scroll.io/api",
-	"8453":      "https://api.basescan.org/api",
-	"84531":     "https://api-goerli.basescan.org/api",
-	"84532":     "https://api-sepolia.basescan.org/api",
-	"59144":     "https://api.lineascan.build/api",
-	"324":       "https://block-explorer-api.mainnet.zksync.io/api",
-	"1101":      "https://api-zkevm.polygonscan.com/api",
-	"168587773": "https://api.routescan.io/v2/network/testnet/evm/168587773/etherscan/api",
-	"1088":      "https://api.routescan.io/v2/network/mainnet/evm/1088/etherscan/api",
-
-	// TODO: finsish adding chains
-}
-
 func main() {
 	godotenv.Load()
 	app := pocketbase.New()
